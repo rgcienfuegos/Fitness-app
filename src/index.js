e54a5ca9c7c8d0036d7b4b69dd2aac63ff8a2d9e
Jerson Q <solorsanolopez@gmail.com>
2019-11-28 22:35:04 -0800

    Adding Styles // Learning how to use Props to make reusable components // Using desescructuring EC6 with props

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from './components/card';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const user = {
-  firstName: 'Raul',
-  LastName: 'Palacios',
-  avatar: 'https://s.gravatar.com/avatar/c02f20e05bd274bf4e18067a4196bd54?s=80'
-};
-
-function getName(user) {
-  return `${user.firstName} ${user.LastName}`;
-}
-
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello {getName(user)}</h1>;
-  }
-  return <h1>Hello Strange</h1>;
-}
-
-const name = 'Jerson';
-const element = (
-  <div>
-    <h1>{getGreeting(user)}</h1>
-    <img src={user.avatar} />
-  </div>
-);
 const container = document.getElementById('root');
 
 // ReactDOM.render(___QUE____,__DONDE____);
 
-ReactDOM.render(element, container);
+ReactDOM.render(
+  <Card
+    title="Technique Guides"
+    description="Learn amazing street workout and calisthenics"
+    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+    leftColor="#A74CF2"
+    rightColor="#617BFB"
+  />,
+  container
+);
